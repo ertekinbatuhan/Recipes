b2f6afd6478ec81616099d86d77a6a872e127af0
Batuhan Berk Ertekin <batuhanertekinn@gmail.com>
2024-08-26 19:32:01 +0300

    add APIConstants

---
+++ Recipes/Constants/APIConstants.swift
@@ -0,0 +1,13 @@
+//
+//  APIConstants.swift
+//  Recipes
+//
+//  Created by Batuhan Berk Ertekin on 26.08.2024.
+//
+
+import Foundation
+
+struct APIConstants {
+    static let baseURL = "https://api.spoonacular.com/recipes/complexSearch"
+    static let apiKey = "$$$$$$$$$$$$$$$$$$$$$$$$$$"
+}
